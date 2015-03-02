234dc0e54e43a72f30fb7de46571883b5918cbc7
Robert Wagner <robjustinwagner@users.noreply.github.com>
2015-03-01 19:30:47 -0600

    Update Instance.java

--- src/Instance.java
+++ src/Instance.java
@@ -1,30 +1,33 @@
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Holds data for particular instance.
+/* This class holds information for particular instance
+ * of our data set, including a label and list of attributes
+ * associated with the instance. These instances are collected
+ * in DataSet.java.
+ *
+ * @author Bob (Robert) Wagner
  */
 public class Instance {
 	
-	public String label;
-	public List<String> attributes = null;
+	public String label;			//label of the data instance
+	public List<String> attributes = null;	//list of attributs associated with the data instance
 
-	/**
-	 * Add attribute values in the order of
-	 * attributes as specified by the dataset
+	/* Adds an attribute value for this data instance.
+	 * @param attributeToAdd	the attribute to add.
 	 */
-	public void addAttribute(String i) {
+	public void addAttribute(String attributeToAdd) {
 		if (attributes == null) {
 			attributes = new ArrayList<String>();
 		}
-		attributes.add(i);
+		attributes.add(attributeToAdd);
 	}
 	
-	
-	/**
-	 * Add label value to the instance
+	/* Sets the label value for this data instance.
+	 * @_label	the label value to set
 	 */
 	public void setLabel(String _label) {
 		label = _label;
 	}
+	
 }
