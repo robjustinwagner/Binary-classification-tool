7623a01d06bd9822ddbc91f09b6040d410bc11c0
Robert Wagner <robjustinwagner@users.noreply.github.com>
2015-03-01 20:38:09 -0600

    Update DataSet.java

--- src/DataSet.java
+++ src/DataSet.java
@@ -1,40 +1,50 @@
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * This class organizes the information of a data set into simple structures.
- *
- * Do not modify.
+/* This class organizes the information of a data set 
+ * into simple, constant-access data structures.
  * 
+ * @author Bob (Robert) Wagner
  */
 public class DataSet {
 
-	public List<Instance> instances = null; // ordered list of instances
+	private List<Instance> instances = new ArrayList<Instance>(); // ordered list of instances
 
-	public String [] labels = null;
-	public String [] attr_name = null;		// name of attribute
-	public String [][] attr_val = null;     // candidate values of each attribute
+	private String [] labels = null;  	// instance labels
+	private String [] attr_name = null; 	// attribute names
+	private String [][] attr_val = null;    // candidate values of each attribute
 	
-	/**
-	 * Add instance to collection.
+	/* Adds an instance to the data set collection,
+	 * populating its label value and associated
+	 * attribute list.
+	 * @param line	a line of the data file read in
+	 *		with format:
+	 *			label,attr_1,attr_2,...		
 	 */
 	public void addInstance(String line) {
-		if (instances == null) {
-			instances = new ArrayList<Instance>();
+		Instance instanceToAdd = new Instance();
+		//parse line
+		String[] splitLine = line.split(",");
+		instanceToAdd.setLabel(splitLine[0]);
+		for(int i = 1; i < splitLine.length; i++) {
+			instanceToAdd.addAttribute(splitLine[i]);
 		}
-		Instance instance = new Instance();
-		
-		String[] splitline = line.split(",");
-		for(int i = 1; i < splitline.length; i ++)
-			instance.addAttribute(splitline[i]);
-		instance.setLabel(splitline[0]);
-		
-		instances.add(instance);
+		//add to list of data instances
+		instances.add(instanceToAdd);
 	}
 	
+	/* Adds a specific attribute instance to the data set collection,
+	 * populating its label value and associated
+	 * attribute list.
+	 * @param line	a line of the data file read in
+	 *		with format:
+	 *			label,attr_1,attr_2,...
+	 * @param idx	the index of the attribute in
+	 *		the specified line
+	 */
 	public void addAttribute(String line, int idx) {
-		String[] splitline = line.split(" ");
-		attr_name[idx] = splitline[0];
-		attr_val[idx] = splitline[1].split(",").clone();
+		String[] splitLine = line.split(" ");
+		attr_name[idx] = splitLine[0];
+		attr_val[idx] = splitLine[1].split(",").clone();
 	}
 }
