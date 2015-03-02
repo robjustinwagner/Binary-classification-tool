21151dfc983d5677ee0c61c11796065c5287b846
Robert Wagner <robjustinwagner@users.noreply.github.com>
2015-03-01 19:35:55 -0600

    Update Instance.java

--- src/Instance.java
+++ src/Instance.java
@@ -10,24 +10,37 @@ import java.util.List;
  */
 public class Instance {
 	
-	public String label;			//label of the data instance
-	public List<String> attributes = null;	//list of attributs associated with the data instance
-
+	//label of the data instance
+	private String label;
+	//list of attributes associated with the data instance
+	private List<String> attributes = new ArrayList<String>();
+	
+	/* Gets the label value of this data instance.
+	 * @return	the label value
+	 */
+	public String getLabel() {
+		return label;
+	}
+	
+	/* Sets the label value for this data instance.
+	 * @param _label	the label value to set
+	 */
+	public void setLabel(String _label) {
+		label = _label;
+	}
+	
+	/* Gets the list of attributes associated with this data instance.
+	 * @return	the list of attributes
+	 */
+	public List<String> getAttributes() {
+		return attributes;
+	}
+	
 	/* Adds an attribute value for this data instance.
 	 * @param attributeToAdd	the attribute to add.
 	 */
 	public void addAttribute(String attributeToAdd) {
-		if (attributes == null) {
-			attributes = new ArrayList<String>();
-		}
 		attributes.add(attributeToAdd);
-	}
-	
-	/* Sets the label value for this data instance.
-	 * @_label	the label value to set
-	 */
-	public void setLabel(String _label) {
-		label = _label;
 	}
 	
 }
